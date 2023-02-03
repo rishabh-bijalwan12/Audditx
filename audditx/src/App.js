99a7daaf30aaa52051466ebbca7765ced2f0a0ee
rishabh-bijalwan12 <rishabhbijalwan19@gmail.com>
2023-02-03 17:38:37 +0530

    this is login

--- audditx/src/App.js
+++ audditx/src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import { useState } from 'react';
 import LandingPage from '../src/LandingPage';
 import SignupPage from '../src/components/signup';
+import Loginpage from '../src/components/login';
 // import DesktopLandingpage from './DesktopAssets/DesktopLandingpage';
 
 function App() {
-  const [Login, setLogin] = useState(<SignupPage/>)
+  const [Login, setLogin] = useState(<Loginpage/>)
 
  
 
